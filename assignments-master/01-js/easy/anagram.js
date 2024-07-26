1360c24139283211eeda03cf4c643ad4448a7294
Satakshi Shanvi <satakshi.shanvi@gmail.com>
2024-07-27 00:42:11 +0530

    commit

--- assignments-master/01-js/easy/anagram.js
+++ assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  
+  if(str1.length != str2.length)
+      return false;
 
+  let sortedStr1 = str1.split('').sort().join('');
+  let sortedStr2 = str2.split('').sort().join('');
+
+  return sortedStr1 == sortedStr2;
 }
 
 module.exports = isAnagram;
